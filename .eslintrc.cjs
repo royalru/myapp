4b18869cb626a0a4493121877a88004e67a537a2
royalru <abujahidroyal@gmail.com>
2023-09-10 03:11:51 +0600

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,12 +11,6 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
 
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  plugins: ['react'],
 };
